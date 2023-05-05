4945724fca48082627e9bc979332a69e00dc9b88
AqianYe <2270121117@qq.com>
2023-05-05 17:05:02 +0800

    C语言学习代码

---
+++ test_23_5_3/test_23_5_3/tst.c
@@ -0,0 +1,131 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//void test(int arr[])
+//{
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	test(arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* pc = arr;
+//	printf("%s\n", arr);
+//	printf("%s\n", pc);
+//	printf("%s\n", &arr);
+//	printf("%s\n", &arr[0]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int i = 0;
+//	for (i = 0; i < 6; i++)
+//	{
+//		printf("%c\n", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch[] = "we";
+//	printf("%s\n", &ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* p = "abcdef";
+//	*p = 'w';
+//	printf("%s\n", p);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//	if (arr1 == arr2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//}
+
+//int main()
+//{
+//	char* p1 = "abcdef";
+//	char* p2 = "abcdef";
+//	if (p1 == p2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//}
+
+////指针数组是数组，用来存放指针
+//int main()
+//{
+//	int arr[10] = { 0 };//整型数组
+//	char ch[5] = { 0 };//字符数组
+//	int* parr[4];//存放整型指针的数组 - 指针数组
+//	char* pch[5];//存放字符指针的数组 - 指针数组
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int d = 40;
+//	int* arr[4] = { &a, &b, &c, &d };
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d ", *(arr[i]));
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[] = { 2,3,4,5,6 };
+	int arr3[] = { 3,4,5,6,7 };
+	int* parr[] = { arr1,arr2,arr3 };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", *(parr[i] + j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
